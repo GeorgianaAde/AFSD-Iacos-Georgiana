fa238c298f62ca7af6bb1a4fb4d759b56e6b53ca
Georgiana <stefaniagamesforfun@gmail.com>
2025-10-14 13:40:28 +0300

    laborator1

--- main.py
+++ main.py
@@ -2,3 +2,42 @@ print("ADELA")
 print(34)
 print("ce faci")
 print(97)
+a=20
+numar_variabila=20
+numar_intreg=5
+numar_zecimal=10.5
+print(id(numar_variabila))
+nume_zecimal=10.5
+print(int(numar_zecimal))
+print(str(numar_intreg))
+print(type(numar_intreg))
+print(5**2)
+print(5==8)
+print(9>4)
+print(6//6)
+print("5"==(str(5)))
+
+print(True or False)
+print(not False)
+text_1="vremea este"
+text_2="frumoasa"
+text_final= text_1 + " " +text_2
+print(text_final)
+text_21=" frumoasa"
+print(text_final + " " +text_21 *3)
+text_21=" frumoasa"
+print(text_1[4])
+print(text_1[-1])
+print(text_1[-2])
+lungime_text=len(text_1)
+print(lungime_text)
+subsir=text_1[0:len(text_1):1]
+print(subsir)
+print(text_1.lower())
+print(text_1.upper())
+print(text_1.replace('vremea' ,"copil"))
+print(len(text_1)//2)
+mijloc=len(text_1)//2
+text_1_prima_parte= text_1[0:mijloc:1]
+text_1_doua_parte= text_1[mijloc:1]
+print(text_1_prima_parte)
