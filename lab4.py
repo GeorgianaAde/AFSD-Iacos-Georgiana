7ff043418246054f9a6a6e4280b585b77f772441
Georgiana <georgianaiacos@gmail.com>
2025-11-04 13:45:55 +0200

    lab2

---
+++ lab4.py
@@ -0,0 +1,30 @@
+#Functii sau subprograme
+#sintaxa:
+#def nume_functie():
+    #corpul functiei
+    #scriem codul ce se executa la fiecare apel
+    #return returneaza valoare optional
+def aduna_numere(numar1, numar2):
+    suma= numar1 + numar2
+    return suma
+if __name__== '__main__':
+    print(aduna_numere(numar1: 3, numar2: 8))
+ #functie care verifica daca un numar e prim
+ #parametrii date de intrare: numar
+ #returnam True sau False: daca e prim returnam True daca nu False
+#numaram divizorii nr
+def prim(numar):
+
+print(prim(11))
+#dintr-o lista de numere afisati numerele prime
+lista_de_numere:[10, 20, 23, 42]
+for numar in lista_de_numere:
+    if prim(numar):
+        print(numar)
+# parametrii cu *args numar variabil de parametrii
+def suma_numere(*args):
+    for numar in lista_de numere
+#VARIABILA GLOBALA SI LOCALA
+def f1():
+    a=10 #variabila locala
+    print(f"din interir f1:{a}")
